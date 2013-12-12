13b05631edef58d19296b2e58f453806ed747744
ream <1069700995@qq.com>
2013-12-12 14:07:55 +0800

    step3 done

--- semantic_analysis/semantic_analysis.py
+++ semantic_analysis/semantic_analysis.py
@@ -11,6 +11,8 @@ class semantic_ana():
             self.analysis_tree(self.tree, self.tree[1][0])
             self.analysis_tree2(self.tree)
 
+        self.analysis_tree3(self.tree[1][0])
+
 
     def analysis_tree(self, p_node, node):  # 去除单链
         if len(node[1]) == 1:
@@ -33,12 +35,20 @@ class semantic_ana():
             self.analysis_tree2(node[1][i])
 
     def analysis_tree3(self, node):
-        if node[0] not in ['I', 'I\'']:
-            return
         children = node[1]
-        if children[1]:
-            pass
-
+        for item in children:
+            if item[0] in ['I', 'I\'']:
+                self.analysis_tree3(item)
+        for item in children:
+            if item[0] in ['-','fo', 'f2', 'mo']:
+                children.remove(item)
+                if len(item[1]) == 0:  # item is a leaf node
+                    node[0] = item[0]
+                    #node[2] = item[2]
+                else:                  # item is not a leaf node
+                    node[0] = item[0]
+                    node[1] += item[1]
+                    #node[2] = item[2]
 
 
     def clear_list(self, l):
